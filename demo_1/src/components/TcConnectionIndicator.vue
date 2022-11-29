c681d1459c0b04369b38ede5aee92c70220da96b
Norman <hakim3nor@gmail.com>
2022-11-29 15:32:45 +0800

    Update TcConnectionIndicator.vue

--- demo_1/src/components/TcConnectionIndicator.vue
+++ demo_1/src/components/TcConnectionIndicator.vue
@@ -1,16 +1,33 @@
 <template>
   <div >
-    
+    <p id="status"></p>
+  
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+const checkOnlineStatus = async () => {
+  try {
+    const online = await fetch("https://demo-live-data.highcharts.com/aapl-ohlcv.json");
+    return online.status >= 200 && online.status < 300; // either true or false
+  } catch (err) {
+    return false; // definitely offline
   }
-}
+};
+
+setInterval(async () => {
+  const result = await checkOnlineStatus();
+  const statusDisplay = document.getElementById("status");
+  statusDisplay.textContent = result ? "Online" : "OFFline";
+}, 1000); // probably too often, try 30000 for every 30 seconds
+
+// forgot to include async load event listener in the video! 
+// window.addEventListener("load", async () => {
+//   const statusDisplay = document.getElementById("status");
+//   statusDisplay.textContent = (await checkOnlineStatus())
+//     ? "Online"
+//     : "OFFline";
+// });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
