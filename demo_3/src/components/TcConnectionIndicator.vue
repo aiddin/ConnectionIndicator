5b22dc8c39680b31af9fa51592ea983e35dab66b
Norman <hakim3nor@gmail.com>
2022-12-06 11:09:07 +0800

    Update TcConnectionIndicator.vue

--- demo_3/src/components/TcConnectionIndicator.vue
+++ demo_3/src/components/TcConnectionIndicator.vue
@@ -1,5 +1,6 @@
 <template>
     <div style="color:green">
+        {{servers}}
       <svg @click="toggleDialog" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class=" m-auto h-20  border-2 ">
               <path fill-rule="evenodd" d="M1.371 8.143c5.858-5.857 15.356-5.857 21.213 0a.75.75 0 010 1.061l-.53.53a.75.75 0 01-1.06 0c-4.98-4.979-13.053-4.979-18.032 0a.75.75 0 01-1.06 0l-.53-.53a.75.75 0 010-1.06zm3.182 3.182c4.1-4.1 10.749-4.1 14.85 0a.75.75 0 010 1.061l-.53.53a.75.75 0 01-1.062 0 8.25 8.25 0 00-11.667 0 .75.75 0 01-1.06 0l-.53-.53a.75.75 0 010-1.06zm3.204 3.182a6 6 0 018.486 0 .75.75 0 010 1.061l-.53.53a.75.75 0 01-1.061 0 3.75 3.75 0 00-5.304 0 .75.75 0 01-1.06 0l-.53-.53a.75.75 0 010-1.06zm3.182 3.182a1.5 1.5 0 012.122 0 .75.75 0 010 1.061l-.53.53a.75.75 0 01-1.061 0l-.53-.53a.75.75 0 010-1.06z" clip-rule="evenodd" />
           </svg>
@@ -22,7 +23,7 @@
     },
     data(){
         return{
-          visible: true,
+          visible: false,
           servers:[
       {
           type: "kaprao",
@@ -55,16 +56,48 @@
           ],
           status: 2
       }
-      
+    ],
+    servers_2:[
+      {
+          type: "kaprao",
+          httpServer: "httpdddddddddds://kaprao-dev.asiaebroker.com",
+          websocketServer: "ws://45.112.197.233:29999/ws-api/v1",
+          supportExchanges: [
+              "KL"
+          ],
+          status: 0
+      },
+      {
+          type: "njqc",
+          httpServer: "httpddddddds://nginx-uat.asiaebroker.com/ath/njqc",
+          websocketServer: null,
+          supportExchanges: [
+              "MY",
+              "SG",
+              "HK"
+          ],
+          status: 1
+      },
     
-    ]
+    ],
+    
         }
+      },
+      created(){
+        this.load()
       },
       methods: {
           toggleDialog() {
               this.visible = !this.visible;
+          },
+          load(){
+            setInterval(function () {
+                  this.servers=this.servers_2
+                }, 1000);
           }
-      }
+          
+      },
+      
   }
   </script>
   
