d75f3189cdf195e509d869d1f482ace1f78e9298
Norman <hakim3nor@gmail.com>
2022-12-16 15:47:05 +0800

    a

---
+++ test/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <br/>
+<v-row
+    justify="center"
+    align="center"
+  >
+    <v-chip draggable>
+      Default
+    </v-chip>
+  </v-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    
+  }),
+}
+</script>
---
+++ vuetify/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+ <v-row
+    justify="center"
+    align="center"
+  >
+    <v-chip draggable>
+      Default
+    </v-chip>
+  </v-row>
+  </div>
+</template>
+
+<script>
+
+
+import "vuetify/dist/vuetify.min.css";
+
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
