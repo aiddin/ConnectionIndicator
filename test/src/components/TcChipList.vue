212e9aa5ebcb70824654cebf55107dd0501ba7e2
Norman <hakim3nor@gmail.com>
2022-12-21 14:42:22 +0800

    Update TcChipList.vue

--- test/src/components/TcChipList.vue
+++ test/src/components/TcChipList.vue
@@ -1,6 +1,5 @@
 <template>
 <div>
-
     <v-chip-group selected-class="text-primary" column>
         <v-chip draggable  @dragover="allowDrop" @dragstart="draggedData=exchange" @drop="onDrop(exchange)" v-for="exchange in exchanges" :key="exchange">
             {{ exchange }}
@@ -17,15 +16,12 @@ export default {
     ],
     data() {
         return {
-
             chip: 'test'
-
         }
     },
     methods: {
         onDrop() {
             this.chip = this.draggedData;
-
         },
         allowDrop(e) {
             e.preventDefault();
